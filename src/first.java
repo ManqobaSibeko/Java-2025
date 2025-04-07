3863cab8c800f06ed6c56d353e2f75e5dc75fd39
Manqoba Sibeko <manqsibeko@gmail.com>
2025-04-07 16:45:01 +0200

    init commit

---
+++ src/first.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class first {
+
+    public static void main(String[] args) {
+
+        // ternary Operator
+
+        int clintAge = 67;
+
+        String ageOfClient = (clintAge > 18) ? "still a kid": "old enough";
+            System.out.println(ageOfClient);
+        System.out.println("");
+    }
+}
